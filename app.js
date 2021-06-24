3dc202c5c5fcb616d4184506cb4b527d6bbf30f6
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-24 20:28:36 +0200

    js_studies

---
+++ app.js
@@ -0,0 +1,65 @@
+// 'use strict'
+
+// let a;
+// let aa;
+// let aaa;
+
+// function add (a,b){
+//     console.log(a+b)
+// }
+
+//  add(3,4)
+
+// let abc = function add (a,b){
+//     console.log(a+b)
+// }
+
+// abc(3,4)
+
+// function stack (){
+//     stack()
+// }
+// stack()
+
+// stack overfall
+
+// var a = 6;
+// var b = 2;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a**b)
+
+// var x = 6;
+// var y = 3;
+
+// x = --y   // bu satirda y = 2
+// console.log({x}) //  bu satirda y = 2
+// console.log({y})
+
+// var c = 6;
+// var d = 3;
+
+// c = d--  // bu satirda d = 3
+// console.log({c})  // bu satirda d = 2
+// console.log({d})
+
+var b= 5;
+
+// console.log(++b) // 6
+// console.log(+b) // 5
+// console.log({b}) // 6
+
+// var a = ++b // b=6
+// console.log({b}) // b=6
+// console.log({a}) // a=6
+
+a = b++
+var a = ++b // a=b, b=b+1
+console.log({b}) // 6
+console.log({a}) //5
+
+
+
